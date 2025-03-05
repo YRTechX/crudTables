11b29d7fe0cd89ee8831e0b7afcfb21488665b0b
Yehor Rud <yrtechx@gmail.com>
2025-03-05 20:27:25 +0200

    fix for vuex types + config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import vuetify from '@/plugins/vuetify.ts'
+import { store, key } from './store'
 
 const app = createApp(App)
 
-app.use(router).use(vuetify).mount('#app')
+app.use(router).use(vuetify).use(store, key).mount('#app')

a54a2bc90e655e125210af3b068295bca3970164
Yehor Rud <yrtechx@gmail.com>
2025-03-05 13:11:54 +0200

    created routes for projects, tasks and 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import type { RouteRecordRaw } from 'vue-router'
+import ProjectsView from '@/views/ProjectsView.vue'
+import ProjectDetailView from '@/views/ProjectDetailView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    redirect: '/projects',
+  },
+  {
+    path: '/projects',
+    name: 'home',
+    component: ProjectsView,
+  },
+  {
+    path: '/projects/:projectId',
+    name: 'projectDetail',
+    component: ProjectDetailView,
+    props: true,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'notFound',
+    component: NotFoundView,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  routes,
 })
 
 export default router

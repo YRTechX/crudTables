97fad132af72bff7f1314e9e6f540b4cb3269b4d
Yehor Rud <yrtechx@gmail.com>
2025-03-05 22:13:48 +0200

    fixes for 2307 ts error + env file settings

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+/// <reference types="vite/types/importMeta.d.ts" />
+interface ImportMetaEnv {
+  readonly VITE_BASE_API_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    VITE_BASE_API_URL: string
+  }
+}

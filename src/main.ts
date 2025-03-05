f1f4ca23c050842688ca5f4be45c79a051f93309
Yehor Rud <yrtechx@gmail.com>
2025-03-05 11:49:16 +0200

    vuetify config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+import vuetify from '@/plugins/vuetify.ts'
 
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app.use(router).use(vuetify).mount('#app')
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,13 @@
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Components
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+export default vuetify

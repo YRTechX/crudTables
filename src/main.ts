f73a85c1ea1f706605f565593424dff2bf0f6e2d
Yehor Rud <yrtechx@gmail.com>
2025-03-06 18:52:43 +0200

    added  vue toastification library and enabled it

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import vuetify from '@/plugins/vuetify.ts'
+import { Toast, options } from '@/plugins/toastification'
 import { store, key } from './store'
 
 const app = createApp(App)
 
-app.use(router).use(vuetify).use(store, key).mount('#app')
+app.use(router).use(vuetify).use(store, key).use(Toast, options).mount('#app')
---
+++ src/plugins/toastification.ts
@@ -0,0 +1,9 @@
+import Toast, { PluginOptions } from 'vue-toastification'
+
+import 'vue-toastification/dist/index.css'
+
+const options: PluginOptions = {
+  // You can set your default options here
+}
+
+export { Toast, options }
